235f6043cb6442d80b1953a329a5d54fdd4bc0a1
MRafliS71 <mraflis@upi.edu>
2022-06-01 22:20:50 +0700

    Update

--- frontend/index.php
+++ frontend/index.php
@@ -1,3 +1,7 @@
+<?php
+  $con = new mysqli('localhost', 'root', '', 'rpl_tubes');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -6,6 +10,7 @@
     <title>Visitor Counter</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css" />
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <style>
       .banner-image {
         background-image: url('./Assets/image/jica1.png');
@@ -17,7 +22,7 @@
     <!-- Navbar  -->
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-5 mx-auto justify-content-center">
       <div class="">
-        <a class="" href="index.html">
+        <a class="" href="index.php">
           <img src="./Assets/image/logo-white.png" alt="Logo" style="width: 200px;"> 
         </a>
         <button
@@ -35,7 +40,7 @@
           <div class=""></div>
           <ul class="my-3 navbar-nav row">
             <li class="col-6">
-              <a class="nav-item nav-active" href="#">Home</a>
+              <a class="nav-item nav-active" href="index.php">Home</a>
             </li>
             <li class="col-6">
               <a class="nav-item" href="about.html">About</a>
@@ -50,16 +55,42 @@
         <div class="divider1 col"></div>
         <div class="visitor-text text-center">
           <h5 class="text1">Pengunjung yang mengunjungi gedung hari</h5>
-          <h5>Selasa, 24 Mei 2022</h5>
+          <h5 >
+          <?php
+            date_default_timezone_set("Asia/Jakarta");
+            echo date("l").", ".date("d - m - Y");
+          ?>
+          </h5>
         </div>
         <div class="content text-center">
-          <h1 class="text-white visitor-count">000</h1>
+          <h1 class="text-white visitor-count" id="container"></h1>
         </div>
-        <div class="row justify-content-center my-3">
-          <div class="col-3 rounded-pill text-center" style="background-color: #FF4646;">
-            <h4 class="py-2" style="color: white;">Penuh</h4>
-          </div>
-        </div>
+        <?php
+          /*
+          if($data['jml_pengunjung'] <= 60){
+            echo'
+            <div class="row justify-content-center my-3">
+              <div class="col-3 rounded-pill text-center" style="background-color: #8CFF79;">
+                <h4 class="py-2" style="color: Black;">Senggang</h4>
+              </div>
+            </div>';
+          }else if($data['jml_pengunjung'] > 60){
+            echo'
+            <div class="row justify-content-center my-3">
+              <div class="col-3 rounded-pill text-center" style="background-color: #FFF281;">
+                <h4 class="py-2" style="color: Black;">Normal</h4>
+              </div>
+            </div>';
+          }else if($data['jml_pengunjung'] >= 260){
+            echo'
+            <div class="row justify-content-center my-3">
+              <div class="col-3 rounded-pill text-center" style="background-color: #FF4646;">
+                <h4 class="py-2" style="color: white;">Penuh</h4>
+              </div>
+            </div>';
+          }*/
+        ?>
+        
       </div>
     </div>
 
@@ -74,8 +105,9 @@
         </div>
 
         <div class="container py-5">
-          <div class="d-flex justify-content-center py-3">
-            <img src="./Assets/image/sample-stat.png" alt="">
+          <div class="d-flex justify-content-center py-3 rounded-3" style="background-color: white;">
+            <!-- grafik line chart -->
+            <canvas id="myChart"></canvas>
           </div>
           <div class="row justify-content-center py-3" style="color: white;">
             <div class="col-8">
@@ -119,7 +151,7 @@
     <section style="background-color: #525252;">
       <div class="container">
         <div class="d-flex justify-content-center">
-          <a class="my-3" href="index.html">
+          <a class="my-3" href="index.php">
             <img src="./Assets/image/logo-white.png" alt="Logo" style="width: 120px;"> 
           </a>
         </div>
@@ -132,6 +164,7 @@
       </div>
     </section>
 
+    <!-- Script -->
     <script src="js/bootstrap.bundle.min.js"></script>
     <script type="text/javascript">
       var nav = document.querySelector('nav');
@@ -143,6 +176,55 @@
           nav.classList.remove('bg-dark', 'shadow');
         }
       });
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        function bisa() {
+            <?php
+            $query = $con->query("SELECT * FROM t_pengunjung");
+
+            foreach ($query as $data) {
+                $month[] = $data['waktu_pengunjung'];
+                $amount[] = $data['jml_pengunjung'];
+            }
+            ?>
+            const xhttp = new XMLHttpRequest();
+            xhttp.onload = function() {
+                document.getElementById("container").innerHTML = this.responseText;
+            }
+
+            if (Math.floor(Math.random() * 10) >= 0 && Math.floor(Math.random() * 10) <= 4) { //genap
+                xhttp.open("GET", "masuk.php");
+                xhttp.send();
+            } else {
+                xhttp.open("GET", "keluar.php");
+                xhttp.send();
+            }
+            const labels = <?php echo json_encode($month) ?>;
+            const data = {
+                labels: labels,
+                datasets: [{
+                    label: 'Pengunjung',
+                    data: <?php echo json_encode($amount) ?>,
+                    fill: false,
+                    borderColor: '#FF4646',
+                    tension: 0.1,
+                    pointRadius: 0
+                }]
+            };
+            const config = {
+                type: 'line',
+                data: data,
+            };
+
+            var myChart = new Chart(
+                document.getElementById('myChart'),
+                config
+            );
+        }
+        setInterval(function() {
+            bisa();
+        }, Math.floor(Math.random() * 10000));
     </script>
   </body>
 </html>
---
+++ frontend/masuk.php
@@ -0,0 +1,20 @@
+<?php
+
+include("conf.php");
+include("includes/Template.class.php");
+include("includes/DB.class.php");
+include("includes/Pengunjung.php");
+
+$pengunjung = new Pengunjung($db_host, $db_user, $db_pass, $db_name);
+
+$pengunjung->open();
+$pengunjung->in();
+$pengunjung->latest();
+$data_utama = $pengunjung->getResult();
+echo $data_utama['jml_pengunjung'];
+// $data_utama = "hallo";
+$pengunjung->close();
+
+// $tpl = new Template("templates/index.html");
+// // $tpl->replace("UTAMA", $data_utama);
+// $tpl->write();
